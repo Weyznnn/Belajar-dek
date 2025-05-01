a6baa8fc6bc1e3b99a18016fe0d25e84c8289670
Rafa N.A <rafaninoadiyatma0980@gmail.com>
2025-05-01 10:48:55 +0700

    pembuatan awal

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+#define PI 3.14159
+short jariJari = 0;
+
+int main()
+{
+    cout << "Masukkan jari jari lingkaran: ";
+    cin >> jariJari;
+    double hasil = PI * pow(jariJari, 2);
+    cout << setprecision(5) << hasil;
+    return 0;
+}
