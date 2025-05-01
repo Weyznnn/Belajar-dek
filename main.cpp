12949cbbc0793247f196e766f42323ccfad7e987
Rafa N.A <rafaninoadiyatma0980@gmail.com>
2025-05-01 10:54:43 +0700

    merapikan code menjadi lebih enak dibaca

--- main.cpp
+++ main.cpp
@@ -10,6 +10,6 @@ int main()
     cout << "Masukkan jari jari lingkaran: ";
     cin >> jariJari;
     double hasil = PI * pow(jariJari, 2);
-    cout << setprecision(5) << hasil;
+    cout << setprecision(5) << "hasil: " << hasil;
     return 0;
 }
