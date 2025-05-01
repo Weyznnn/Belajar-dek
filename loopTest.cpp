9dcf3d7172a372f6bf5393ab6a412cb359fb9512
Rafa N.A <rafaninoadiyatma0980@gmail.com>
2025-05-01 22:03:25 +0700

    Hanya ifelse

---
+++ loopTest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int age = 21;
+
+int main() {
+    if(age < 21){
+        cout << "No beer for you little guy";
+    } else{
+        cout << "Enjoy your beer folks";
+    }
+}
