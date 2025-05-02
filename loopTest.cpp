25fc18466b0fe7afd887f2af38ca22a31ea7dee4
Rafa N.A <rafaninoadiyatma0980@gmail.com>
2025-05-02 14:21:36 +0700

    Menambahkan waktu untuk anak di bawah umur

--- loopTest.cpp
+++ loopTest.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int age = 21;
+int age = 18;
+
+int timePased(){
+    while(age < 21) {
+        age ++;
+        cout << "Your age has increase to " << age << endl;
+    } return 0;
+}
 
 int main() {
-    if(age < 21){
-        cout << "No beer for you little guy";
+    if(age < 21) {
+        cout << "No beer for you little guy \n";
+        timePased();
+        cout << "Now you can enjoy the beer \n";
     } else{
         cout << "Enjoy your beer folks";
     }
+    return 0;
 }
